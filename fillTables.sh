a03feb8d9c46397a65a947582dee2bafee440f7b
Patryk <dolata.patryk1@gmail.com>
2020-01-17 22:03:20 +0100

    fillTables poprawione

--- fillTables.sh
+++ fillTables.sh
@@ -15,7 +15,7 @@ spark-submit --class Authorities --master yarn --num-executors 5 --driver-memory
     --executor-memory 512m --executor-cores 1 authorities.jar projekt/input/uk-trafic/
 echo "zasilono tabele authorities"
 
-spark-submit --class Facts --master yarn --num-executors 5 --driver-memory 512m \
-    --executor-memory 512m --executor-cores 1 facts.jar projekt/input/uk-trafic/
+spark-submit --class Facts --master yarn --num-executors 5 --driver-memory 1024m \
+    --executor-memory 1024m --executor-cores 1 facts.jar projekt/input/uk-trafic/
 echo "zasilono fakty"
     
